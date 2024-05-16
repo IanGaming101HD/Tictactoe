3884a05fb842e114fa8c81fd4ddaed05770f87dd
IanGaming101HD <100050322+IanGaming101HD@users.noreply.github.com>
2024-05-16 19:52:49 +0100

    a

--- src/main.js
+++ src/main.js
@@ -9,27 +9,9 @@ app.set('etag', false);
 app.use('/public', express.static(__dirname + '/public'))
 
 app.get('/', async (req, res) => {
-  let file = fs.readFileSync(__dirname + '/public/home/index.html', {
+  let file = fs.readFileSync(__dirname + '/public/main/index.html', {
     encoding: 'utf-8'
   })
-  // file = file.replace('word', 'word2')
-  res.send(file);
-});
-
-app.get('/:page', (req, res, next) => {
-  let page = req.params.page;
-
-  let file;
-
-  try {
-    file = fs.readFileSync(__dirname + `/public/${page}/index.html`, {
-      encoding: 'utf-8'
-    })
-  } catch (err) {
-    res.redirect(301, '/error')
-  }
-  // check this ^^
-
   res.send(file);
 });
 
@@ -37,5 +19,3 @@ app.listen(port, () => {
   console.log(`http://localhost:${port}/`);
   console.log(`App listening on port ${port}`);
 });
-
-// require('./discord-bot/main.js');
